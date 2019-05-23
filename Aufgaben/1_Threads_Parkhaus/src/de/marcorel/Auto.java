4ad4464b2ebc5699d17be685afb11b0b6a3de049
kalle111 <marc1.orel@st.oth-regensburg.de>
2019-05-23 15:39:41 +0200

    Aufgabe 1a geloest

---
+++ Aufgaben/1_Threads_Parkhaus/src/de/marcorel/Auto.java
@@ -0,0 +1,51 @@
+package de.marcorel;
+
+import java.util.Random;
+
+public class Auto extends Thread {
+    //Auto-Klasse
+    //private String Kennzeichen;
+    private Parkhaus parkhaus;
+
+    public Auto(String kennzeichen, Parkhaus ph) {
+        super(kennzeichen); // Weitergabe an Konstruktor der Klasse Thread => deswegen extends Thread
+        this.parkhaus = ph;
+    }
+
+    @Override
+    public void run() {
+        // Starte Thread für 1 Auto
+
+        //ZufallsZahlenGenerator erstellen
+        Random ZufallsZahl = new Random();
+
+        while(true) {
+            //Auto soll ständig ein und aus fahren!
+
+            //Auto soll aber randomisierte Zeit warten, vor der nächsten Aktion.
+            int fahrzeit = ZufallsZahl.nextInt(3);
+
+            //Thread soll <<fahrzeit>> lang schlafen => Falls interrupted während dem Schlaf, wird eine Exception gecatched.
+            try {
+                Thread.sleep((fahrzeit*1000));
+            } catch (InterruptedException e) {
+                System.out.println("Thread wurde während dem Fahren aufgeweckt. Beende Thread.");
+                return; //beendet den Thread
+            }
+            //Nachdem zufällig lang gefahren wurde wird nun der Einparkvorgange eingeleitet
+            this.parkhaus.einfahren();
+
+            // Erstellung der Parkzeit, wenn eingeparkt wurde (try/catch)
+            int parkzeit = ZufallsZahl.nextInt(20);
+            try {
+                Thread.sleep(parkzeit*1000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread wurde während dem Parken aufgeweckt => wird beendet.");
+                return;
+            }
+            this.parkhaus.ausfahren();
+        }
+
+
+    }
+}
---
+++ Aufgaben/1_Threads_Parkhaus/src/de/marcorel/Simulation.java
@@ -0,0 +1,18 @@
+package de.marcorel;
+
+public class Simulation {
+
+    public static void main(String[] args) {
+	// write your code here
+        //System.out.println("Hello World");
+
+        //Erstellung Parkhaus
+        Parkhaus ph = new Parkhaus(5);
+
+        //Erstellung Autos
+        for(int i = 0; i<20; i++){
+            Thread t = new Auto("NM-OTHR-" + (i+1), ph);
+            t.start();
+        }
+    }
+}
